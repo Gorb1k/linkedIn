db2d8f4fff9bdab2b9d9ec13201ac9f2c65f64c5
Ihor Horb <gorbigorolegovich@gmail.com>
2021-09-20 13:29:45 +0300

    final

--- src/components/widgets/Widgets.js
+++ src/components/widgets/Widgets.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Widgets.css'
+import InfoIcon from '@material-ui/icons/Info'
+import RecordIcon from '@material-ui/icons/FiberManualRecord'
 
 const Widgets = () => {
+
+    const newsArticle = (heading, subtitle) => {
+      return (
+          <div className='widgets__article'>
+              <div className="widgets__articleLeft">
+                  <RecordIcon/>
+              </div>
+              <div className="widgets__articleRight">
+                  <h4>{heading}</h4>
+                  <p>{subtitle}</p>
+              </div>
+          </div>
+      )
+    }
+
     return (
-        <div>
-            
+        <div className='widgets'>
+            <div className="widgets__header">
+                <h2>LinkedIn news</h2>
+                <InfoIcon/>
+            </div>
+            {newsArticle('React News', 'Hola!')}
         </div>
     );
 };
